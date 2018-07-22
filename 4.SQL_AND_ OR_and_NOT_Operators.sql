6f2c366d7ddd52e570d480a4afc6db3c048762d9
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-22 15:59:01 +0600

    Create 4.SQL_AND_ OR_and_NOT_Operators.sql

---
+++ 4.SQL_AND_ OR_and_NOT_Operators.sql
@@ -0,0 +1,27 @@
+The SQL AND, OR and NOT Operators
+The WHERE clause can be combined with AND, OR, and NOT operators.
+The AND and OR operators are used to filter records based on more than one condition:
+The AND operator displays a record if all the conditions separated by AND is TRUE.
+The OR operator displays a record if any of the conditions separated by OR is TRUE.
+The NOT operator displays a record if the condition(s) is NOT TRUE.
+
+  AND Syntax
+  SELECT column1, column2, ...
+  FROM table_name
+  WHERE condition1 AND condition2 AND condition3 ...;
+  
+  OR Syntax
+  SELECT column1, column2, ...
+  FROM table_name
+  WHERE condition1 OR condition2 OR condition3 ...;
+  
+  NOT Syntax
+  SELECT column1, column2, ...
+  FROM table_name
+  WHERE NOT condition;
+  
+  SELECT * FROM Customers
+  WHERE Country='Germany' AND City='Berlin';
+  
+  SELECT * FROM Customers
+  WHERE City='Berlin' OR City='München';
